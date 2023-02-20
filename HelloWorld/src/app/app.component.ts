12aa1f9591c3b2fd366654a19e526a2857db7c8a
miodraggosic <gosmio88@gmail.com>
2023-02-20 15:18:55 +0100

    feat: Candidate Class method for print info

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -12,15 +12,35 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     //Task 1
-    const candidate1 = new Candidate('Miodrag', 'Gosic', new Date(1999, 9, 9));
+    const candidate1 = new Candidate('Miodrag', 'Gosic', new Date(1988, 9, 12));
     const candidate2 = new Candidate('Julijus', 'Mejn', new Date(1985, 4, 4));
+    const candidate3 = new Candidate(
+      'Petar',
+      'Petrovic',
+      new Date(1962, 5, 23)
+    );
 
     console.log(candidate1, candidate2);
 
     //Task2
 
-    const candidates: SingleCandidate[] = [candidate1, candidate2];
+    const candidates: SingleCandidate[] = [candidate1, candidate2, candidate3];
 
     console.log(candidates);
+
+    //Task 3
+
+    console.log(this.printCandidate(candidates));
+  }
+
+  printCandidate(candidates: SingleCandidate[]): string {
+    let display: string = '';
+    for (const candidate of candidates) {
+      display += ` ${candidate.firstName} ${
+        candidate.lastName
+      }: ${candidate.birthday.toDateString()};`;
+    }
+
+    return display;
   }
 }
