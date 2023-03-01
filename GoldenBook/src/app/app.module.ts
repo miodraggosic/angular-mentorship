a9f1ab7f553abe049a1ac5f5b69b91546b4a73cd
miodraggosic <gosmio88@gmail.com>
2023-03-01 02:30:54 +0100

    feat: create library component

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { LibraryComponent } from './features/library/library.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LibraryComponent],
   imports: [BrowserModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
