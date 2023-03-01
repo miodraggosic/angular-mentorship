801926b627752e221dd52641b16f91fcc00302ca
miodraggosic <gosmio88@gmail.com>
2023-03-01 03:17:37 +0100

    update: footer text added

--- GoldenBook/src/app/core/components/footer/footer.component.ts
+++ GoldenBook/src/app/core/components/footer/footer.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  text: string = 'Orion Inovation - ANGULAR WORKSHOP 2023';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
