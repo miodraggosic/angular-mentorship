4c5a066f3863506ca86fd5efce97667827b6effb
miodraggosic <gosmio88@gmail.com>
2023-03-01 02:51:39 +0100

    feat: create search component

--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { BookComponent } from './components/book/book.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
 @NgModule({
   declarations: [
     CardComponent,
-    BookComponent
+    BookComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
