15ac89bb930a5d1ede5d788d0456cb13e6481fee
miodraggosic <gosmio88@gmail.com>
2023-03-02 03:22:45 +0100

    update: search component logic added

--- GoldenBook/src/app/shared/components/search/search.component.ts
+++ GoldenBook/src/app/shared/components/search/search.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  @Output() displayTerm = new EventEmitter<string>();
 
-  constructor() { }
+  term: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onClick(): void {
+    console.log(this.term);
+
+    this.displayTerm.emit(this.term);
   }
 
+  searchTerm(event: any): void {
+    this.term = event.target.value;
+  }
 }
