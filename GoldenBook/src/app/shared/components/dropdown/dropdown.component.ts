7caf361ea3e3978257f0156a09e74838ebc9efdc
miodraggosic <gosmio88@gmail.com>
2023-03-02 03:52:06 +0100

    update: selectedCategory method added

--- GoldenBook/src/app/shared/components/dropdown/dropdown.component.ts
+++ GoldenBook/src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Categories } from 'src/app/models/enums/categories.enum';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
 })
 export class DropdownComponent implements OnInit {
+  @Output() displayCategory = new EventEmitter<string>();
 
-  constructor() { }
+  categoryValue: string = '';
 
-  ngOnInit(): void {
+  categoriesFilter: Categories[] = [
+    Categories.general,
+    Categories.fantasy,
+    Categories.history,
+    Categories.sciense,
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectedCategory(event: any): void {
+    this.categoryValue = event.target.value;
+    this.displayCategory.emit(this.categoryValue);
   }
-
 }
