8e493284cb607215b1cbc42b42324475b30a2768
miodraggosic <gosmio88@gmail.com>
2023-03-02 01:40:54 +0100

    update: books array added

--- GoldenBook/src/app/features/library/library.component.ts
+++ GoldenBook/src/app/features/library/library.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Categories } from 'src/app/models/enums/category.enum';
+import { Book } from 'src/app/models/interfaces/book.interface';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.scss']
+  styleUrls: ['./library.component.scss'],
 })
 export class LibraryComponent implements OnInit {
+  books: Book[] = [
+    {
+      imageUrl: '../../../../assets/images/books/moneyPower.png',
+      title: 'Money Power',
+      author: 'Name Surname',
+      year: '1998-01-30',
+      category: Categories.general,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum?',
+    },
+    {
+      imageUrl: '../../../assets/images/books/myFirstLover.png',
+      title: 'My First Love',
+      author: 'Name Surname',
+      year: '2000-12-04',
+      category: Categories.general,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum?',
+    },
+    {
+      imageUrl: '../../../assets/images/books/myLittleLife.png',
+      title: 'My Little Life',
+      author: 'Name Surname',
+      year: '2005-08-10',
+      category: Categories.general,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum?',
+    },
+    {
+      imageUrl: '../../../assets/images/books/theLastTrip.png',
+      title: 'The Lost Trip',
+      author: 'Name Surname',
+      year: '2008-07-07',
+      category: Categories.fantasy,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum.',
+    },
+    {
+      imageUrl: '../../../assets/images/books/dark.png',
+      title: 'Dark',
+      author: 'Name Surname',
+      year: '2018-12-04',
+      category: Categories.fantasy,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum.',
+    },
+    {
+      imageUrl: '../../../assets/images/books/findMe.png',
+      title: 'Find Me',
+      author: 'Name Surname',
+      year: '2021, 10, 15',
+      category: Categories.general,
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure facere molestiae ducimus commodi dignissimos corrupti accusantium deserunt minima enim dicta assumenda sapiente quam, dolorum eum ipsa voluptatem sed, consequatur eligendi perspiciatis, non soluta? Eum sequi, ab cupiditate omnis, animi doloremque amet modi natus rem, aperiam tempore laborum ipsa voluptatum.',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
