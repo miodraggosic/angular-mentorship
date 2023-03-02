3a9a8bdc30eb8be64efe646aa09bd7de45d28d16
miodraggosic <gosmio88@gmail.com>
2023-03-02 01:39:43 +0100

    update: book Input added

--- GoldenBook/src/app/shared/components/card/card.component.ts
+++ GoldenBook/src/app/shared/components/card/card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/interfaces/book.interface';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input() book!: Book;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
