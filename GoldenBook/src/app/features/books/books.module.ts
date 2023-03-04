ae63e8930909c502a63f752fac63c4613e200cc1
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:07:33 +0100

    update: books.modul created

---
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -0,0 +1,9 @@
+import { BooksComponent } from './components/books/books.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [BooksComponent],
+  imports: [CommonModule],
+})
+export class BooksModule {}
