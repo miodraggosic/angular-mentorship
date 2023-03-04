e22902732e48db82e97504b4f7899093ccb84d26
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:21:01 +0100

    update: book component moved to books modul

---
+++ GoldenBook/src/app/features/books/components/book/book.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/interfaces/book.interface';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+})
+export class BookComponent implements OnInit {
+  @Input() book!: Book;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './components/card/card.component';
-import { BookComponent } from './components/book/book.component';
 import { SearchComponent } from './components/search/search.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [
-    CardComponent,
-    BookComponent,
-    SearchComponent,
-    DropdownComponent,
-  ],
+  declarations: [SearchComponent, DropdownComponent],
   imports: [CommonModule],
-  exports: [CardComponent, BookComponent, SearchComponent, DropdownComponent],
+  exports: [SearchComponent, DropdownComponent],
 })
 export class SharedModule {}
