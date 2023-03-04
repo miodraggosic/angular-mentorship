584ee61fa8f7d7bc261c5ea9e668a73cbe5e771c
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:19:26 +0100

    update: book component added, books exported

--- GoldenBook/src/app/features/books/books.module.ts
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -1,9 +1,11 @@
 import { BooksComponent } from './components/books/books.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
-  declarations: [BooksComponent],
+  declarations: [BooksComponent, BookComponent],
   imports: [CommonModule],
+  exports: [BooksComponent],
 })
 export class BooksModule {}
