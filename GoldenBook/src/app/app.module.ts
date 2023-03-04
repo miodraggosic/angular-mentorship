48da179dfe9ca1ae1d18ee323fdf4cf233ebe47e
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:19:58 +0100

    update: books modul imported

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BooksModule } from './features/books/books.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,8 +8,8 @@ import { SharedModule } from './shared/shared.module';
 import { BooksComponent } from './features/books/components/books/books.component';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent],
-  imports: [BrowserModule, CoreModule, SharedModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, SharedModule, BooksModule],
   providers: [],
   bootstrap: [AppComponent],
 })
