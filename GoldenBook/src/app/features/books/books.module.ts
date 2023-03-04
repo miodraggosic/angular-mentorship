bf148a0a9d1074b9ec6c9e9146d501b137c63616
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:26:55 +0100

    update: shared modul import

--- GoldenBook/src/app/features/books/books.module.ts
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../shared/shared.module';
 import { BooksComponent } from './components/books/books.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,7 +6,7 @@ import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [BooksComponent, BookComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [BooksComponent],
 })
 export class BooksModule {}
