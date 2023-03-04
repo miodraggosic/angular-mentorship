9bae91e812005cf4e322f4ebb8f3ebf631511125
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:08:56 +0100

    update: library renamed to books component

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { LibraryComponent } from './features/library/library.component';
+import { BooksComponent } from './features/books/components/books/books.component';
 
 @NgModule({
-  declarations: [AppComponent, LibraryComponent],
+  declarations: [AppComponent, BooksComponent],
   imports: [BrowserModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
