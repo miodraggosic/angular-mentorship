ffd167a45fcbd88fb99296f9c77edb1225fe7213
miodraggosic <gosmio88@gmail.com>
2023-03-04 03:27:49 +0100

    update: unused imports deleted

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BooksModule } from './features/books/books.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { BooksComponent } from './features/books/components/books/books.component';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule, SharedModule, BooksModule],
