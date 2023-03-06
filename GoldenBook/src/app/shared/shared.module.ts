e92fb46e37b4441cd06f209c6b9049486664d12c
miodraggosic <gosmio88@gmail.com>
2023-03-06 18:35:19 +0100

    update: pagination component create & style added

--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { ButtonComponent } from './components/button/button.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
-  declarations: [SearchComponent, DropdownComponent, ButtonComponent],
+  declarations: [
+    SearchComponent,
+    DropdownComponent,
+    ButtonComponent,
+    PaginationComponent,
+  ],
   imports: [CommonModule],
-  exports: [SearchComponent, DropdownComponent, ButtonComponent],
+  exports: [
+    SearchComponent,
+    DropdownComponent,
+    ButtonComponent,
+    PaginationComponent,
+  ],
 })
 export class SharedModule {}
