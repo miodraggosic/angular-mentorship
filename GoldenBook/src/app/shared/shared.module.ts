f41ee52a35de67c1525f0b5fb3655224ccc6bc5b
miodraggosic <gosmio88@gmail.com>
2023-03-06 17:46:03 +0100

    update: button component created and style added

--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [SearchComponent, DropdownComponent],
+  declarations: [SearchComponent, DropdownComponent, ButtonComponent],
   imports: [CommonModule],
-  exports: [SearchComponent, DropdownComponent],
+  exports: [SearchComponent, DropdownComponent, ButtonComponent],
 })
 export class SharedModule {}
