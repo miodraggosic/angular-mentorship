7e9f411b9bd93dbf867e76cf53da7af223430dd3
miodraggosic <gosmio88@gmail.com>
2023-03-08 17:36:31 +0100

    update: book to use card component from material

--- GoldenBook/src/app/features/books/books.module.ts
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -3,10 +3,11 @@ import { BooksComponent } from './components/books/books.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookComponent } from './components/book/book.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [BooksComponent, BookComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, MatCardModule],
   exports: [BooksComponent],
 })
 export class BooksModule {}
