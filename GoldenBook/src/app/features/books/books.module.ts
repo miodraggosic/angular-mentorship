6d99865db4378f97b621240ae72f633eff073a56
miodraggosic <gosmio88@gmail.com>
2023-03-09 02:05:15 +0100

    update: Book and Search components  to books modul

--- GoldenBook/src/app/features/books/books.module.ts
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -3,11 +3,28 @@ import { BooksComponent } from './components/books/books.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookComponent } from './components/book/book.component';
+import { SearchComponent } from './components/search/search.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [BooksComponent, BookComponent],
-  imports: [CommonModule, SharedModule, MatCardModule],
+  declarations: [
+    BooksComponent,
+    BookComponent,
+    SearchComponent,
+    DropdownComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   exports: [BooksComponent],
 })
 export class BooksModule {}
--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchComponent } from './components/search/search.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { ButtonComponent } from './components/button/button.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
-  declarations: [
-    SearchComponent,
-    DropdownComponent,
-    ButtonComponent,
-    PaginationComponent,
-  ],
+  declarations: [ButtonComponent, PaginationComponent],
   imports: [CommonModule],
-  exports: [
-    SearchComponent,
-    DropdownComponent,
-    ButtonComponent,
-    PaginationComponent,
-  ],
+  exports: [ButtonComponent, PaginationComponent],
 })
 export class SharedModule {}
