aa187bb6293ad4d0aeec821a51ada501ff257d06
miodraggosic <gosmio88@gmail.com>
2023-03-13 03:21:56 +0100

    fix: use Paginator from material #13

--- GoldenBook/src/app/shared/components/pagination/pagination.component.ts
+++ GoldenBook/src/app/shared/components/pagination/pagination.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent implements OnInit {
+  length: number = 104;
+  pageSizeOptions: number[] = [6, 12, 24];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [ButtonComponent, PaginationComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule],
   exports: [ButtonComponent, PaginationComponent],
 })
 export class SharedModule {}
