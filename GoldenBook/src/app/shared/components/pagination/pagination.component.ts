800e442061b0a6553d576e96d0aa5d3a58281bc2
miodraggosic <gosmio88@gmail.com>
2023-03-15 21:08:09 +0100

    fix:  Pagination label changed  #25

--- GoldenBook/src/app/shared/components/pagination/pagination.component.ts
+++ GoldenBook/src/app/shared/components/pagination/pagination.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, AfterViewInit {
+  @ViewChild('pagination') paginator!: MatPaginator;
+
   length: number = 104;
   pageSizeOptions: number[] = [6, 12, 24];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.paginator._intl.itemsPerPageLabel = 'Rows per page:';
+  }
 }
