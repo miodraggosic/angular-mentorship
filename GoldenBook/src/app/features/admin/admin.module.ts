e92506a427f9a9b04c835b7b45a75363de1083da
miodraggosic <gosmio88@gmail.com>
2023-03-23 00:19:52 +0100

    update: Admin component created

--- GoldenBook/src/app/features/admin/admin.module.ts
+++ GoldenBook/src/app/features/admin/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminBooksComponent } from './components/admin-books/admin-books.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminBooksComponent
+  ],
   imports: [
     CommonModule
   ]
