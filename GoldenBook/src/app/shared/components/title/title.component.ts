a019446893e77230fed63ae389e0bd3cf34abd2a
miodraggosic <gosmio88@gmail.com>
2023-03-23 00:25:37 +0100

    update: Title component to receive title text

--- GoldenBook/src/app/shared/components/title/title.component.ts
+++ GoldenBook/src/app/shared/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent implements OnInit {
-  title: string = 'Homepage';
+  @Input() title?: string;
 
   constructor() {}
 
