07e803ed7f397cc2c0610e8ad51dc59db2d80113
miodraggosic <gosmio88@gmail.com>
2023-03-29 19:24:36 +0200

    fix: Books component removed from exports #37

--- GoldenBook/src/app/features/books/books.module.ts
+++ GoldenBook/src/app/features/books/books.module.ts
@@ -1,10 +1,8 @@
-import { SharedModule } from './../../shared/shared.module';
-import { BooksComponent } from './components/books/books.component';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './../../shared/shared.module';
 import { CommonModule } from '@angular/common';
-import { BookComponent } from './components/book/book.component';
-import { SearchComponent } from './components/search/search.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { BooksRoutingModule } from './books-routing.module';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -12,9 +10,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatChipsModule } from '@angular/material/chips';
+
+import { BooksComponent } from './components/books/books.component';
+import { BookComponent } from './components/book/book.component';
+import { SearchComponent } from './components/search/search.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { BookOverviewComponent } from './components/book-overview/book-overview.component';
-import { BooksRoutingModule } from './books-routing.module';
+
 import { BookBackgroundDirective } from './directives/book-background.directive';
+
 @NgModule({
   declarations: [
     BooksComponent,
@@ -36,6 +40,6 @@ import { BookBackgroundDirective } from './directives/book-background.directive'
     MatChipsModule,
     BooksRoutingModule,
   ],
-  exports: [BooksComponent],
+  exports: [],
 })
 export class BooksModule {}
