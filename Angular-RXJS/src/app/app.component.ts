99bed08a7f0e29d8ebdd2cd22e56ed8b2eafea71
miodraggosic <gosmio88@gmail.com>
2023-04-07 14:16:46 +0200

    fix: issues  #39, #57 , #58

--- Angular-RXJS/src/app/app.component.ts
+++ Angular-RXJS/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ajax } from 'rxjs/ajax';
 import {
+  Observable,
+  OperatorFunction,
+  Subject,
   catchError,
   combineLatest,
   debounceTime,
@@ -8,15 +10,15 @@ import {
   fromEvent,
   interval,
   map,
-  Observable,
   of,
+  pipe,
   retry,
-  Subject,
   takeUntil,
   timer,
 } from 'rxjs';
-import { Words } from './models/interfaces/words.interface';
+import { ajax } from 'rxjs/ajax';
 import { Teams } from './models/interfaces/teams.interface';
+import { Words } from './models/interfaces/words.interface';
 
 @Component({
   selector: 'app-root',
@@ -24,15 +26,10 @@ import { Teams } from './models/interfaces/teams.interface';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private intervalSubscription$ = new Subject<boolean>();
-  private timerSubscription$ = new Subject<boolean>();
-  private unsuscribe$ = new Subject<boolean>();
+  //Task1
+  curentTime$!: Observable<string>;
 
-  curentTime$: Observable<string> = interval(1000).pipe(
-    map(() => new Date().toLocaleTimeString()),
-    takeUntil(this.intervalSubscription$)
-  );
-
+  //Task2
   searchInput$!: Observable<string>;
 
   //Task3
@@ -58,92 +55,28 @@ export class AppComponent implements OnInit, OnDestroy {
   timer$!: Observable<number>;
   count!: number;
 
+  private intervalSubscription$ = new Subject<boolean>();
+  private timerSubscription$ = new Subject<boolean>();
+  private unsuscribe$ = new Subject<boolean>();
+
   ngOnInit(): void {
     //Task1
-    this.curentTime$.subscribe((value) => console.log(value));
+    this.logCurentTime();
 
     //Task 2
-    this.searchInput$ = fromEvent(
-      document.getElementById('input-search1')!,
-      'change'
-    ).pipe(
-      map((elem: Event) => (elem.target as HTMLInputElement).value),
-      debounceTime(500),
-      takeUntil(this.unsuscribe$)
-    );
-
-    this.searchInput$.subscribe((elem: string) =>
-      console.log('Search Input:', elem)
-    );
+    this.logSearch();
 
     //Task3
-    this.input1$ = fromEvent(
-      document.getElementById('input-search2')!,
-      'change'
-    ).pipe(map((elem) => (elem.target as HTMLInputElement).value));
-
-    this.input2$ = fromEvent(
-      document.getElementById('input-search3')!,
-      'change'
-    ).pipe(map((elem) => (elem.target as HTMLInputElement).value));
-
-    this.result$ = combineLatest([this.input1$, this.input2$]).pipe(
-      map(([input1, input2]) => input1 + input2),
-      takeUntil(this.unsuscribe$)
-    );
-
-    this.result$.subscribe((value) => {
-      console.log('Combined value:', value);
-    });
+    this.logCombinedSearch();
 
     //Task 4
-    this.data1$ = ajax<Words>(
-      'https://api.datamuse.com/words?ml=ringing+in+the+ears'
-    ).pipe(map((data) => data.response));
-
-    this.data2$ = ajax<Teams>('https://www.balldontlie.io/api/v1/teams').pipe(
-      map((data) => data.response)
-    );
-
-    forkJoin([this.data1$, this.data2$])
-      .pipe(takeUntil(this.unsuscribe$))
-      .subscribe((value) => console.log('Api response data:', value));
+    this.logDataForkJoin();
 
     //Task 5
-    this.inputNumber1$ = fromEvent(
-      document.getElementById('input-search4')!,
-      'change'
-    ).pipe(map((input) => Number((input.target as HTMLInputElement).value)));
-
-    this.inputNumber2$ = fromEvent(
-      document.getElementById('input-search5')!,
-      'change'
-    ).pipe(map((input) => Number((input.target as HTMLInputElement).value)));
-
-    this.sum$ = combineLatest([this.inputNumber1$, this.inputNumber2$]).pipe(
-      map(([input1, input2]) => input1 + input2),
-      takeUntil(this.unsuscribe$)
-    );
-
-    this.sum$.subscribe((value) => console.log('Sum of values:', value));
+    this.logCombinedNumbers();
 
     //Task 6
-    this.data$ = ajax<Teams>('https://www.balldontlie.io/api/v1/team').pipe(
-      map((data) => {
-        if (!data) {
-          throw new Error();
-        }
-        return data.response;
-      }),
-      retry(3),
-      catchError((err) => {
-        console.log('Error receiving data');
-        return of(err.message);
-      }),
-      takeUntil(this.unsuscribe$)
-    );
-
-    this.data$.subscribe((value) => console.log(value));
+    this.logDataError();
   }
 
   ngOnDestroy(): void {
@@ -164,6 +97,108 @@ export class AppComponent implements OnInit, OnDestroy {
   stopTimer() {
     this.timerSubscription$.next(true);
     this.timerSubscription$.complete();
+  }
+
+  // Helper Functions
+  private mapInput(): OperatorFunction<Event, string> {
+    return pipe(map((elem: Event) => (elem.target as HTMLInputElement).value));
+  }
+  private mapInputNumbers(): OperatorFunction<Event, number> {
+    return pipe(
+      map((elem: Event) => Number((elem.target as HTMLInputElement).value))
+    );
+  }
+
+  private logCurentTime(): void {
+    this.curentTime$ = interval(1000).pipe(
+      map(() => new Date().toLocaleTimeString()),
+      takeUntil(this.intervalSubscription$)
+    );
+    this.curentTime$.subscribe((value) => console.log(value));
+  }
+
+  private logSearch() {
+    this.searchInput$ = fromEvent(
+      document.getElementById('input-search1')!,
+      'change'
+    ).pipe(this.mapInput(), debounceTime(500), takeUntil(this.unsuscribe$));
+
+    this.searchInput$.subscribe((elem: string) =>
+      console.log('Search Input:', elem)
+    );
+  }
+
+  private logCombinedSearch() {
+    this.input1$ = fromEvent(
+      document.getElementById('input-search2')!,
+      'change'
+    ).pipe(this.mapInput(), takeUntil(this.unsuscribe$));
+
+    this.input2$ = fromEvent(
+      document.getElementById('input-search3')!,
+      'change'
+    ).pipe(this.mapInput(), takeUntil(this.unsuscribe$));
+
+    this.result$ = combineLatest([this.input1$, this.input2$]).pipe(
+      map(([input1, input2]) => input1 + input2),
+      takeUntil(this.unsuscribe$)
+    );
+
+    this.result$.subscribe((value) => {
+      console.log('Combined value:', value);
+    });
+  }
+
+  private logDataForkJoin() {
+    this.data1$ = ajax<Words>(
+      'https://api.datamuse.com/words?ml=ringing+in+the+ears'
+    ).pipe(map((data) => data.response));
+
+    this.data2$ = ajax<Teams>('https://www.balldontlie.io/api/v1/teams').pipe(
+      map((data) => data.response)
+    );
+
+    forkJoin([this.data1$, this.data2$])
+      .pipe(takeUntil(this.unsuscribe$))
+      .subscribe((value) => console.log('Api response data:', value));
+  }
+
+  private logCombinedNumbers() {
+    this.inputNumber1$ = fromEvent(
+      document.getElementById('input-search4')!,
+      'change'
+    ).pipe(this.mapInputNumbers(), takeUntil(this.unsuscribe$));
+
+    this.inputNumber2$ = fromEvent(
+      document.getElementById('input-search5')!,
+      'change'
+    ).pipe(this.mapInputNumbers(), takeUntil(this.unsuscribe$));
+
+    this.sum$ = combineLatest([this.inputNumber1$, this.inputNumber2$]).pipe(
+      map(([input1, input2]) => input1 + input2),
+      takeUntil(this.unsuscribe$)
+    );
+
+    this.sum$.subscribe((value) => console.log('Sum of values:', value));
+  }
+
+  private logDataError() {
+    this.data$ = ajax<Teams>('https://www.balldontlie.io/api/v1/team').pipe(
+      map((data) => {
+        if (!data) {
+          throw new Error();
+        }
+        return data.response;
+      }),
+      retry(3),
+      catchError((err) => {
+        console.log('Error receiving data');
+        return of(err.message);
+      }),
+      takeUntil(this.unsuscribe$)
+    );
+
+    this.data$.subscribe((value) => console.log(value));
   }
 
   private unsubscribe() {
