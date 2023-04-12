5a8b5df9c78abe95445169d4f069a02f17ea0e2e
miodraggosic <gosmio88@gmail.com>
2023-04-12 18:36:54 +0200

    update: softDelete & handleError  methods added

--- GoldenBook/src/app/features/books/services/books.service.ts
+++ GoldenBook/src/app/features/books/services/books.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, map } from 'rxjs';
+import { Observable, of, map, catchError, retry } from 'rxjs';
 import { Book } from 'src/app/models/interfaces/book.interface';
 import { books } from '@mocks/mock-books';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '@env';
 
 @Injectable({
@@ -11,18 +11,28 @@ import { environment } from '@env';
 export class BooksService {
   private books: Book[] = books;
 
+  private booksUrl: string = `${environment.baseApiUrl}books`;
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(private httpClientService: HttpClient) {}
 
   getAll(): Observable<Book[]> {
     return this.httpClientService
-      .get<Book[]>(`${environment.baseApiUrl}books?deletedAt=null`)
-      .pipe(map((data) => data));
+      .get<Book[]>(`${this.booksUrl}?deletedAt=null`)
+      .pipe(
+        map((data) => data),
+        retry(2),
+        catchError(this.handleError('Get books', []))
+      );
   }
 
-  delete(id: number): Observable<{}> {
-    return this.httpClientService.delete(
-      `${environment.baseApiUrl}books/${id}`
-    );
+  delete(id: number): Observable<any> {
+    return this.httpClientService
+      .delete(`${this.booksUrl}/${id}`)
+      .pipe(catchError(this.handleError('Delete', [])));
   }
 
   getById(id: number): Observable<Book | undefined> {
@@ -30,5 +40,22 @@ export class BooksService {
       map((books) => books.find((book) => book.id === id))
     );
     return book;
+  }
+
+  softDelete(id: number): Observable<any> {
+    const date: string = new Date().toISOString();
+    const body = {
+      deletedAt: date,
+    };
+    return this.httpClientService
+      .patch(`${this.booksUrl}/${id}`, body, this.httpOptions)
+      .pipe(catchError(this.handleError('Soft delete', [])));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(operation, error.message);
+      return of(result as T);
+    };
   }
 }
