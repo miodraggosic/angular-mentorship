dc317c3f3e0961110981cf2961ecf4a9b0405e1d
miodraggosic <gosmio88@gmail.com>
2023-04-24 16:43:20 +0200

    update: app-table component added

--- GoldenBook/src/app/features/admin/components/admin-books/admin-books.component.ts
+++ GoldenBook/src/app/features/admin/components/admin-books/admin-books.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-books',
   templateUrl: './admin-books.component.html',
-  styleUrls: ['./admin-books.component.scss']
+  styleUrls: ['./admin-books.component.scss'],
 })
-export class AdminBooksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AdminBooksComponent {}
