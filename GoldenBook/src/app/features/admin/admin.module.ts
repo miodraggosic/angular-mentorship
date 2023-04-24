287220eae70815567242f6d1f0b8765ed15714a5
miodraggosic <gosmio88@gmail.com>
2023-04-24 16:41:11 +0200

    update: import MatTableModule

--- GoldenBook/src/app/features/admin/admin.module.ts
+++ GoldenBook/src/app/features/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminBooksComponent } from './components/admin-books/admin-books.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatTableModule } from '@angular/material/table';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AdminBooksComponent],
-  imports: [CommonModule, SharedModule, AdminRoutingModule],
+  declarations: [AdminBooksComponent, TableComponent],
+  imports: [CommonModule, SharedModule, AdminRoutingModule, MatTableModule],
   exports: [],
 })
 export class AdminModule {}
