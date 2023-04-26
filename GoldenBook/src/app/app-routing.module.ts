a1555f58a25eaddc84bab78e94743b56484718b6
miodraggosic <gosmio88@gmail.com>
2023-04-26 22:48:25 +0200

    update: Login component created & routing updated

--- GoldenBook/src/app/app-routing.module.ts
+++ GoldenBook/src/app/app-routing.module.ts
@@ -2,22 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { BooksLayoutComponent } from './core/layouts/books-layout/books-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'books',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
-    path: 'books',
-    loadChildren: () =>
-      import('./features/books/books.module').then((m) => m.BooksModule),
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path: 'admin',
-    loadChildren: () =>
-      import('./features/admin/admin.module').then((m) => m.AdminModule),
+    path: 'homepage',
+    component: BooksLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./features/books/books.module').then((m) => m.BooksModule),
+      },
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('./features/admin/admin.module').then((m) => m.AdminModule),
+      },
+    ],
   },
   {
     path: '**',
--- GoldenBook/src/app/core/core.module.ts
+++ GoldenBook/src/app/core/core.module.ts
@@ -7,17 +7,24 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { LoginComponent } from './auth/components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BooksLayoutComponent } from './layouts/books-layout/books-layout.component';
 
 const COMPONENTS = [
   HeaderComponent,
   FooterComponent,
   SidenavComponent,
   PageNotFoundComponent,
+  LoginComponent,
+  BooksLayoutComponent,
 ];
 
 @NgModule({
@@ -31,6 +38,9 @@ const COMPONENTS = [
     MatListModule,
     MatIconModule,
     SharedModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: COMPONENTS,
 })
