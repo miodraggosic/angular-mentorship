13f03c5856a37a9d6ba84dd2a998b51c6845c431
miodraggosic <gosmio88@gmail.com>
2023-04-26 02:04:57 +0200

    fix: MatDialogModule added to imports

--- GoldenBook/src/app/app.module.ts
+++ GoldenBook/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
