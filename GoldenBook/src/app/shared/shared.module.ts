be9f1da699738c7c8e6fbf65b5529718301cf493
miodraggosic <gosmio88@gmail.com>
2023-04-30 08:57:32 +0200

    update: ConfirmDialog added and modul imports

--- GoldenBook/src/app/shared/shared.module.ts
+++ GoldenBook/src/app/shared/shared.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TitleComponent } from './components/title/title.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
-const COMPONENTS = [ButtonComponent, PaginationComponent, TitleComponent];
+const COMPONENTS = [
+  ButtonComponent,
+  PaginationComponent,
+  TitleComponent,
+  ConfirmDialogComponent,
+];
 
 @NgModule({
   declarations: COMPONENTS,
-  imports: [CommonModule, MatPaginatorModule],
+  imports: [CommonModule, MatPaginatorModule, MatDialogModule, MatButtonModule],
   exports: COMPONENTS,
 })
 export class SharedModule {}
