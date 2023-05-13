bd437230bdc5a64bffb28a2a86507ff1e8159b69
miodraggosic <gosmio88@gmail.com>
2023-05-13 22:58:08 +0200

    fix:  FieldBorderDirective logic adjusted

--- GoldenBook/src/app/shared/directives/field-border.directive.ts
+++ GoldenBook/src/app/shared/directives/field-border.directive.ts
@@ -3,18 +3,20 @@ import {
   Directive,
   ElementRef,
   HostListener,
+  Input,
 } from '@angular/core';
 
 @Directive({
   selector: '[appFieldBorder]',
 })
 export class FieldBorderDirective implements AfterViewInit {
-  private value = '';
+  @Input() value!: string;
 
   constructor(private elementRef: ElementRef) {}
 
   @HostListener('keyup', ['$event']) onKeyup(event: KeyboardEvent): void {
     this.value = (event.target as HTMLInputElement).value;
+
     this.setBorderColor(this.value);
   }
 
@@ -24,10 +26,14 @@ export class FieldBorderDirective implements AfterViewInit {
 
   private setBorderColor(value: string) {
     const elem = this.elementRef.nativeElement as HTMLInputElement;
+
     if (value.trim() === '') {
-      elem.classList.add('red');
+      // elem.classList.add('red');
+      elem.style.border = '2px solid red';
       return;
     }
-    elem.classList.replace('red', 'green');
+    elem.style.border = '2px solid green';
+
+    // elem.classList.replace('red', 'green');
   }
 }
